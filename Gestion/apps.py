b7dad9ae940aba3454333347909c78af6b15ea90
Wapo-lang <cdavid107@hotmail.com>
2025-12-10 19:16:33 -0500

    Biblioteca Django

--- Gestion/apps.py
+++ Gestion/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GestionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Gestion'
+
+    def ready(self):
+        import Gestion.signals
